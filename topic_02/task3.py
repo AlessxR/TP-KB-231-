2629ae96e8a34605f4458fa2a655ad7c2181d50a
Alexxoooo <alexandaliev33@gmail.com>
2024-09-22 14:40:52 +0300

    add tasks for topic 02

---
+++ topic_02/task3.py
@@ -0,0 +1,47 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "На нуль ділити не можна"
+
+def checkOper(operation):
+    return operation in "+-*/"
+
+def calc():
+    while True:
+        expression = input("Введіть 'exit' для виходу з программи або будь-яке число: ")
+        if expression.lower() == "exit":
+            print("бай-бай")
+            break
+        
+        num1 = float(expression)
+        operation = input("Введіть операцію (+, -, *, /): ")
+        
+        if not checkOper(operation):
+            print("Неправильна операція")
+            continue
+        
+        num2 = float(input("Введіть друге число: "))
+
+        match operation:
+            case '+':
+                result = add(num1, num2)
+            case '-':
+                result = subtract(num1, num2)
+            case '*':
+                result = multiply(num1, num2)
+            case '/':
+                result = divide(num1, num2)
+
+        print("Result =", result)
+
+calc()
